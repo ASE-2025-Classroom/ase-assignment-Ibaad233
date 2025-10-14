3c6276e9e838a72942a1281fe22e95ac3992c4d2
hp <ibaad007123@outlook.com>
2025-10-14 15:11:48 +0100

    test

--- BOOSEprograms/BOOSEprograms/obj/Release/net8.0-windows/BOOSEprograms.AssemblyInfo.cs
+++ BOOSEprograms/BOOSEprograms/obj/Release/net8.0-windows/BOOSEprograms.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BOOSEprograms")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dbacd7f7cab3bf906b3568db6e3785a82a0385b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BOOSEprograms")]
